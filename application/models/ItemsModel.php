140bcd46a67cecf1b699b40894340f40d860e8ee
yungga123 <53594401+yungga123@users.noreply.github.com>
2020-01-22 16:02:31 +0800

    Update ItemsModel.php

--- application/models/ItemsModel.php
+++ application/models/ItemsModel.php
@@ -49,6 +49,10 @@ class ItemsModel extends CI_Model {
 		return $this->db->query("UPDATE items SET stocks = stocks + ".$stocksToAdd." WHERE itemCode = '".$itemCode."'");
 	}
 
+
+	public function count_items() {
+		
+	}
 	// *****************SERVER SIDE VALIDATION FOR DATATABLE*********************
 	var $table = "items as a";
 	//var $join_table = "customer_vt as b";
