67de2a43c97efb6dc6358f18fa9eb38c69e55e31
Sooyaa <cendricktrilles@gmail.com>
2021-07-14 09:05:55 +0800

    Sales Inquiry Updates:071421
    1. Hide Generate Report Button in Existing Clients

--- application/views/sales_inquiry/existing_client_view.php
+++ application/views/sales_inquiry/existing_client_view.php
@@ -21,9 +21,11 @@ defined('BASEPATH') or die('Access Denied'); ?>
 					<div class="card">
 						<div class="card-header">
 							Clients Table
-								<div class="float-right">
+							<!--	
+							<div class="float-right">
 								<a href="<?php echo site_url('exportexistingclientsproject/') ?>" class="btn btn-danger btn-sm"> <i class="fas fa-file-excel"></i> Generate Report</a>
 								</div>
+							-->
                         </div>
 
 						<div class="card-body">
