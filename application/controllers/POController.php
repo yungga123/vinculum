c781f2b38d2848755ab72f9a6183fbe37c30e9ee
Sooyaa <cendricktrilles@gmail.com>
2021-08-03 09:53:21 +0800

    Generate PO Updates:080321
    1. Change to ID for fetching datas

--- application/controllers/POController.php
+++ application/controllers/POController.php
@@ -262,10 +262,7 @@ class POController extends CI_Controller {
 			[
 				'field' => 'attention_to',
 				'label' => 'Name of Addressee',
-				'rules' => 'trim|required',
-				'errors' => [
-					'required' => 'Please Enter Name of Addressee'
-				]
+				'rules' => 'trim'
             ]
         ];
         
