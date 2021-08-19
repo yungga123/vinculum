2acd91a5e9200135b990bffff0fc37e7e778de4f
Sooyaa <cendricktrilles@gmail.com>
2021-08-19 19:00:10 +0800

    url updates
    ip routes

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/vinculum';
+$config['base_url'] = 'http://223.130.19.88:8080/vinculum';
 
 /*
 |--------------------------------------------------------------------------
