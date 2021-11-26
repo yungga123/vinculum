271312d2dc7599ec918606887953247ce190b689
Sooyaa <cendricktrilles@gmail.com>
2021-11-26 13:37:47 +0800

    Sales Inquiry Updates:112621
    1. Add Sir Rod to Sales List in project edit form

--- application/views/sales_inquiry/project_view.php
+++ application/views/sales_inquiry/project_view.php
@@ -327,7 +327,7 @@ if ($this->session->userdata('add')) {
                                                     <label for="project_sales_incharge">Sales Incharge</label>
                                                     <select name="project_sales_incharge" class="form-control">
                                                         <?php foreach ($edit_sales_list as $row) : ?>
-                                                            <?php if ($row->id == "01021415" || $row->id == "PTS09092020" || $row->id == "02021415" || $row->id == "24120518" || $row->id == "PS021021") : ?>
+                                                            <?php if ($row->id == "01021415" || $row->id == "PTS09092020" || $row->id == "02021415" || $row->id == "24120518" || $row->id == "PS021021" || $row->id == "SSC091321") : ?>
                                                                 <option value="<?php echo $row->id ?>" <?php if ($project_data_result['project_sales_edit'] == $row->id) {
                                                                                                             echo 'selected';
                                                                                                         } ?>>
