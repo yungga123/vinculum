24168f80ce9dee3872eda99b6a315762760ad17a
yungga123 <53594401+yungga123@users.noreply.github.com>
2022-12-07 14:10:36 +0800

    Base URL Config
    1. from 192.168.0.90/vinculum to 61.28.136.219:8080/vinculum base path

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://192.168.0.90/vinculum';
+$config['base_url'] = 'http://61.28.136.219:8080/vinculum';
 // 223.130.19.88:8080
 
 
