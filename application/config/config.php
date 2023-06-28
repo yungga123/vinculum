62f1351e86c977bc08a7ef47bc3b4a484d4917f5
yungga123 <53594401+yungga123@users.noreply.github.com>
2023-06-28 16:15:55 +0800

    change server

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://localhost/vinculum';
+//$config['base_url'] = 'http://61.28.136.219:8080/vinculum';
+$config['base_url'] = 'http://mis.old';
 // 223.130.19.88:8080
 
 
